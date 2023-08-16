4ac0137559109ef7f048bb0d96a6df7de0c9923f
fernando-iwerson <fernandoiwerson@gmail.com>
2023-08-16 02:04:36 -0300

    first commit

---
+++ matrizBidimesional.js
@@ -0,0 +1,9 @@
+let animais = [
+    ["Cachorro","Viralata",3],
+    ["Passaro","Papa-Capim",1],
+    ["Peixe","Salmão",7]
+];
+
+console.log(`nome: ${animais[0][0]} / Espécie: ${animais[0][1]} / idade: ${animais[0][2]}`);
+console.log(`nome: ${animais[1][0]} / Espécie: ${animais[1][1]} / idade: ${animais[1][2]}`);
+console.log(`nome: ${animais[2][0]} / Espécie: ${animais[2][1]} / idade: ${animais[2][2]}`);
